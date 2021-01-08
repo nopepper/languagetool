ce1eb4546c536733c4565af5a7137a6c664548a0
ihatepepperballs <robert.veres00@gmail.com>
2021-01-08 11:51:00 +0100

    Lower epsilon to 1e-5

--- languagetool-core/src/main/java/org/languagetool/language/NGramLangIdentifier.java
+++ languagetool-core/src/main/java/org/languagetool/language/NGramLangIdentifier.java
@@ -32,7 +32,7 @@ import static org.languagetool.language.LanguageIdentifier.canLanguageBeDetected
 
 public class NGramLangIdentifier {
 
-  private final static double EPSILON = 1e-4;
+  private final static double EPSILON = 1e-5;
 
   private final Map<String, Integer> vocab;
   private final List<String[]> codes; // Elem format = {Name, 2-code (or "NULL"), 3-code}
