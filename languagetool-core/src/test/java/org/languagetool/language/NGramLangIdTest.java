2bf0e674d8a9fc02c32a7a96ef8c107edca4fe92
ihatepepperballs <robert.veres00@gmail.com>
2021-01-30 19:52:24 +0100

    Remove redundant "Throws IOException" from NGramLangIdTest intializer

--- languagetool-core/src/test/java/org/languagetool/language/NGramLangIdTest.java
+++ languagetool-core/src/test/java/org/languagetool/language/NGramLangIdTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.*;
 import static org.junit.Assert.assertTrue;
 
 public class NGramLangIdTest {
-  public NGramLangIdTest() throws IOException {
+  public NGramLangIdTest() {
   }
 
   private class ProcessedSentence {
